3b2d5af3e6812a80d094bd0bf93b5fc4ae40f631
Raseboka Nelson <rasebokanelson1@gmail.com>
2026-06-03 20:57:46 +0200

    Create Coding.sql

---
+++ Coding.sql
@@ -0,0 +1,31 @@
+SELECT *
+FROM retail_sales;
+
+--Display only the customer ID and product category columns
+SELECT `Customer ID`,
+`Product Category`
+FROM retail_sales;
+
+--Display all electronic transactions
+SELECT *
+FROM retail_sales
+WHERE `Product Category` = 'Electronics';
+
+--Display transactions where the quantity purchased is 3 or more
+SELECT *
+FROM retail_sales
+WHERE Quantity >3;
+
+--Display Beauty products sales where the total amount is greater than 100
+SELECT *
+FROM retail_sales
+WHERE `Product Category` = 'Beauty' AND `Total Amount` > 100;
+
+--Display customers whose ages are between 25 and 35
+SELECT*
+FROM retail_sales
+WHERE Age BETWEEN 25 AND 35;
+
+--Calculate the total sales amount for all transactions
+SELECT SUM(`Total Amount`) AS total_transactions
+FROM retail_sales;
